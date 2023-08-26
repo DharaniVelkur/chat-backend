5495f80931a8b38a437fe46b062d719f1b28feff
Velkur Dharani <dharanivelkur@gmail.com>
2023-08-26 17:53:17 +0530

    ..

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server=http.createServer(app);
 
 const io =new Server(server,{
     cors:{ 
-        origin:"https://chat-frontend-henna.vercel.app/",
+        origin:"*",
         methods:["GET","POST"]
     }
 });
